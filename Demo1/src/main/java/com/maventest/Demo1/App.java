dbde64e58bad7bbd1d206b6d6df222963a69ef0f
JG00568405 <JG00568405@INBASDPC09646.TechMahindra.com>
2018-11-07 12:07:36 +0530

    My frist commit

---
+++ Demo1/src/main/java/com/maventest/Demo1/App.java
@@ -0,0 +1,15 @@
+package com.maventest.Demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("hi maven");
+    }
+}
